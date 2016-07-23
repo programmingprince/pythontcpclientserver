19bf713f9d84881f9485506b3b195bd3de0c70c8
Nirajan Timalsina <princenirajan12@gmail.com>
2016-07-23 19:46:17 +0545

    First commit

---
+++ tcpClient.py
@@ -0,0 +1,17 @@
+#this is a tcp client
+
+import socket
+
+SERVER_IP="127.0.0.1"
+PORT=2343
+BUFFER_SIZE=1024
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_address=(SERVER_IP,PORT)
+s.connect(server_address)
+data=input('Enter any text to send...')
+s.send(data.encode(encoding="utf-8"))
+data=s.recv(BUFFER_SIZE)
+content=data.decode("utf-8")
+print('Server:',content)
+s.close()
---
+++ tcpServer.py
@@ -0,0 +1,20 @@
+#this is a tcp server
+
+import socket
+
+SERVER_IP="127.0.0.1"
+PORT=2343
+BUFFER_SIZE=1024
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_address=(SERVER_IP,PORT)
+s.bind(server_address)
+s.listen(1)
+connection, clientAddr=s.accept()
+print("Got connection request from: ",clientAddr)
+data=connection.recv(BUFFER_SIZE)
+content=data.decode("utf-8")
+print('Client:',content)
+data=input('Enter any text to send...')
+connection.send(data.encode(encoding="utf-8"))
+connection.close()
