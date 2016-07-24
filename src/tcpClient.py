1672d0d220f708370c0a68dcc6902f95b13bb766
Nirajan Timalsina <princenirajan12@gmail.com>
2016-07-24 18:56:58 +0545

    Code formatted

--- src/tcpClient.py
+++ src/tcpClient.py
@@ -1,18 +1,18 @@
-#this is a tcp client
+# this is a tcp client
 
 import socket
 
-SERVER_IP="127.0.0.1"
-PORT=2343
-BUFFER_SIZE=1024
+SERVER_IP = "127.0.0.1"
+PORT = 2343
+BUFFER_SIZE = 2048
 
-#creating socket object for client
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_address=(SERVER_IP,PORT)
+# creating socket object for client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (SERVER_IP, PORT)
 s.connect(server_address)
-data=input('Enter any text to send...')
+data = input('Enter any text to send...')
 s.send(data.encode(encoding="utf-8"))
-data=s.recv(BUFFER_SIZE)
-content=data.decode("utf-8")
-print('Server:',content)
+data = s.recv(BUFFER_SIZE)
+content = data.decode("utf-8")
+print('Server:', content)
 s.close()
--- src/tcpServer.py
+++ src/tcpServer.py
@@ -1,21 +1,21 @@
-#this is a tcp server
+# this is a tcp server
 
 import socket
 
-SERVER_IP="127.0.0.1"
-PORT=2343
-BUFFER_SIZE=1024
+SERVER_IP = "127.0.0.1"
+PORT = 2343
+BUFFER_SIZE = 2048
 
-#creating a socket object
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_address=(SERVER_IP,PORT)
+# creating a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (SERVER_IP, PORT)
 s.bind(server_address)
 s.listen(1)
-connection, clientAddr=s.accept()
-print("Got connection request from: ",clientAddr)
-data=connection.recv(BUFFER_SIZE)
-content=data.decode("utf-8")
-print('Client:',content)
-data=input('Enter any text to send...')
+connection, clientAddr = s.accept()
+print("Got connection request from: ", clientAddr)
+data = connection.recv(BUFFER_SIZE)
+content = data.decode("utf-8")
+print('Client:', content)
+data = input('Enter any text to send...')
 connection.send(data.encode(encoding="utf-8"))
 connection.close()
